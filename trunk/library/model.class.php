e0e891b7391ba293d00f5989749fa8926e1b7788
ndusan <ndusan@de4b64f8-2e82-11df-954b-21e382534593>
2010-05-22 12:10:33 +0000

    db communication

--- trunk/library/model.class.php
+++ trunk/library/model.class.php
@@ -12,10 +12,16 @@ class Model{
 	 * Contructor
 	 * @return boolean
 	 */
-	function __construct(){
+	public function __construct(){
 		$this->_dbHandle = @mysql_connect(DB_HOST, DB_USER, DB_PASS);
         if ($this->_dbHandle != 0) {
-            if (mysql_select_db(DB_NAME, $this->_dbHandle)) return true;
+            if (mysql_select_db(DB_NAME, $this->_dbHandle)){
+				@mysql_query("SET NAMES 'utf8'", $this->_dbHandle);
+				@mysql_query("SET CHARACTER_SET_CLIENT=utf8", $this->_dbHandle);
+				@mysql_query("SET CHARACTER_SET_RESULTS=utf8", $this->_dbHandle);
+				@mysql_query("SET CHARACTER_SET_CONNECTION=utf8", $this->_dbHandle);            	 
+            	return true;
+            }
             else return false;
         } else return false;
 	}
